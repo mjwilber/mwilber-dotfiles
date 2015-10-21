355e745c80c9f1ff6528db7e05dbe45237caacf7
Matt Wilber <matt.wilber@gmail.com>
2015-10-21 16:37:48 -0500

    Move towards a less hidden file approach to a dotfile repository.

--- .conky/my fave conky/startupconky.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sleep 20 && conky -c ~/.my_fave_conky/.conkyrc_clock;
-sleep 1 && conky -c ~/.my_fave_conky/.conkyrc_sys;
-sleep 1 && conky -c ~/.my_fave_conky/.conkyrc_cpu;
-sleep 1 && conky -c ~/.my_fave_conky/.conkyrc_mem;
-sleep 1 && conky -c ~/.my_fave_conky/.conkyrc_proc;
-sleep 1 && conky -c ~/.my_fave_conky/.conkyrc_hd;
-sleep 1 && conky -c ~/.my_fave_conky/.conkyrc_net;
